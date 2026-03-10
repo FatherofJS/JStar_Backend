c68d5c9ce0ad9c1e3103aa987a2f3f1bb8600557
lduccc <lducc.work@gmail.com>
2026-03-10 16:28:14 +0700

    Initial commit for chatbot

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,5 @@
 from .chart import router as chart_router
 from .location import router as location_router
+from .chat import chatbot
 
-__all__ = ["chart_router", "location_router"]
+__all__ = ["chart_router", "location_router", "chatbot"]
